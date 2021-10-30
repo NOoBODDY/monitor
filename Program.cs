1999852d8c8c6038b20d2f60e35fd7705c0d2773
NOoBODY <offmysoap@gmail.com>
2021-10-30 14:10:18 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace monitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string procesName = args[0];
+            int duration = Convert.ToInt32(args[1]);
+            int clock = Convert.ToInt32(args[2]) * 60 * 1000; // to milliseconds
+
+            bool flag = true;
+            while(flag)
+            {
+                Process[] processes = Process.GetProcessesByName(procesName);
+                if (processes.Length != 0)
+                {
+                    foreach(Process i in processes)
+                    {
+                        if ((DateTime.Now - i.StartTime).TotalMinutes > duration)
+                        {
+                            i.Kill();
+                        }
+                    }
+                    System.Threading.Thread.Sleep(clock);
+
+                }
+                else
+                {
+                    flag = false;
+
+                }
+            }
+
+
+        }
+    }
+}
